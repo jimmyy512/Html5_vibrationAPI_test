d8c65003764ff104fa70508bacfc4c8c00ed51ad
Jim <jimmyy5121@gmail.com>
2018-11-29 16:41:34 +0800

    新增第二種震動方式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,6 +43,9 @@
               <span>Ｙ: {{shakeY.toFixed(2)}}</span>
             </div>
             <div>
+              <span>Ｙ: {{shakeZ.toFixed(2)}}</span>
+            </div>
+            <div>
               <input class="button" type="button" value="點我強制震動" @click="forceShake()">
             </div>
             <div>
@@ -71,16 +74,22 @@ export default {
           isSupportDeviceMotion:false,
           isSupportVibrate:false,
           boxColorIndex:0,
-          speed:15,  // 用来判定的加速度阈值，太大了则很难触发
+          speed:10,  // 用来判定的加速度阈值，太大了则很难触发
           maxShake:0,
           shakeX:0,
           shakeY:0,
+          shakeZ:0,
           x:0,
           y:0,
           z:0,
           lastX:0,
           lastY:0,
           lastZ:0,
+          lastAcc:{
+            alpha:0,
+            beta:0,
+            gamma:0,
+          },
         }
     },
     methods:{
@@ -128,14 +137,17 @@ export default {
         },
         addShakeEvent(){
             if(window.DeviceMotionEvent) {
-              window.addEventListener('devicemotion',this.shakeEvent, false);
+              window.addEventListener('devicemotion',this.shakeDevicemotion, false);
+              // window.addEventListener('deviceorientation',this.shakeDeviceorientation, false);
             }
         },
-        shakeEvent(){
+        shakeDevicemotion()
+        {
             this.isSupportDeviceMotion=true;
             let acceleration = event.accelerationIncludingGravity;
-            this.x = acceleration.x;
-            this.y = acceleration.y;
+            let acc = event.acceleration;
+            this.x = acc.x;
+            this.y = acc.y;
             this.shakeX=Math.abs(this.x-this.lastX);
             this.shakeY=Math.abs(this.y-this.lastY);
             if(this.shakeX>this.maxShake)
@@ -154,6 +166,27 @@ export default {
             }
             this.lastX = this.x;
             this.lastY = this.y;
+        },
+        shakeDeviceorientation(event){
+          let delA = Math.abs(event.alpha - this.lastAcc.alpha);    // alpha轴偏转角
+          let delB = Math.abs(event.beta - this.lastAcc.beta);    // beta轴偏转角
+          let delG = Math.abs(event.gamma - this.lastAcc.gamma);    // gamma轴偏转角
+        
+          if ((delA > this.speed && delB > this.speed) || 
+              (delA > this.speed && delG > this.speed) || 
+              (delB > this.speed || delG > this.speed)) {
+            // 用户设备摇动了，触发响应操作
+            // 此处的判断依据是用户设备的加速度大于我们设置的阈值
+            this.boxColorIndex++;
+            if(this.boxColorIndex==6)
+              this.boxColorIndex=1;
+            if("vibrate" in navigator){
+              navigator.vibrate(700);
+            }
+          }
+          this.shakeX=delA;
+          this.shakeY=delB;
+          this.lastAcc = event;    // 存储上一次的event
         },
     }
 }
